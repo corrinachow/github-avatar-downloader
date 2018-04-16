9aaab1f1790520636630a026054fed2922d2ce0a
corrinachow <corrina.yy.chow@gmail.com>
2018-04-16 15:21:39 -0400

    getRepoContributors prints user.avatar_url

--- download_avatars.js
+++ download_avatars.js
@@ -3,5 +3,22 @@ var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+    headers: {
+      'User-Agent': 'request'
+    }
+  }
+
+  request(options, function(err, res, body) {
+    for (const user of JSON.parse(body)) {
+      console.log(user.avatar_url)
+    }
+
+
+    // cb(err, body);
+  });
 }
+
+
+getRepoContributors('jquery', 'jquery')
